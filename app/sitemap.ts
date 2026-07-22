3c2778ff33c56621d9a7a179736e64d927190e03
alejandroabrilm <d.abrilm@uniandes.edu.co>
2026-07-22 16:01:40 -0500

    change of sitemap.xml to update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://transfroid.com";
+const siteUrl = "https://transfroidfam.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
